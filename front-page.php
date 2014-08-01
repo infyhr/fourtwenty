eef878f798c2731c64e73556ace3760dd748972d
T.B <infy@cybershade.org>
2014-08-01 21:56:04 +0200

    Added a gif of dancing snoop dogge :smile:

--- front-page.php
+++ front-page.php
@@ -3,6 +3,7 @@
 <div class="container">
     <div class="page-header">
         <h1>Sticky footer with fixed navbar</h1>
+        <div class="text-right" style="float: right;"><img src="<?php echo get_template_directory_uri(); ?>/dist/snoop.gif"></div>
     </div>
     <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body > .container</code>.</p>
     <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
