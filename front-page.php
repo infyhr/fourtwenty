d395eb9d95b18cb1850fc41d11323a964f00e98f
T.B <infy@cybershade.org>
2014-09-24 15:45:14 +0200

    Fixed front-page.php syntax for setting the project up

--- front-page.php
+++ front-page.php
@@ -8,7 +8,7 @@
     </div>
     <p class="lead">Looks like everything is working as it should.</p>
     <p>You are currently viewing the default <code>front-page.php</code> made by using Bootstrap's <code>Sticky footer with fixed navbar</code> template.</p>
-    <p>You should probably run <code>420.phar</code> if you haven't already and set this theme up completely.</p>
+    <p>You should probably run <code>$ php 420 setup</code> if you haven't already and set this theme up completely.</p>
     <p class="text-left" style="padding-top: 30px;font-weight: 700;">Good luck and have fun.</p>
     <!-- <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body > .container</code>.</p>
     <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p> -->
